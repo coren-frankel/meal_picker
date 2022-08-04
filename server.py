e6de79071b299ee57fe5d1f8bdb25d29aa55564e
unclebabykern <104476731+unclebabykern@users.noreply.github.com>
2022-08-03 23:15:06 -0600

    Removing Port 5001 for deployment

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask_app import app
 from flask_app.controllers import users
 if __name__ == "__main__":
-    app.run(debug=True,port=5001)
+    app.run(debug=True)#,port=5001)
+    #Port 5001 for debugging only
